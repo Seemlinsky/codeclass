18360724f719b4df16ef2c2371f7947694ed93d0
Seemlinsky <seemlinsky@gmail.com>
2025-12-09 12:41:47 +0100

    init commit :sparkles:

--- Codeclassles1/src/main/java/com/example/codeclassles1/Application.java
+++ Codeclassles1/src/main/java/com/example/codeclassles1/Application.java
@@ -7,4 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage stage) throws IOException {
 
+        Scene scene = new Scene(new VBox(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
